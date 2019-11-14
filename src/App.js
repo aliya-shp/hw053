75fda1cb7f8fbc171ec6781194ae1e054b138f61
Aliya Akmatova <amavarati@gmail.com>
2019-11-14 21:23:51 +0600

    Formed logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import AddTaskForm from "./components/AddTaskForm";
+import nanoid  from 'nanoid'
+import Task from "./components/Task";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tasks: [],
+      currentTask: ''
+    };
+  }
+
+  deleteTask = (id) => {
+    this.id = id;
+    let oldTasks = [...this.state.tasks]
+    let tasks = oldTasks.filter(task => task.id !== id)
+    this.setState({tasks: tasks});
+  };
+
+  addTask = () => {
+    let task = {};
+    task.id = nanoid();
+    task.text = this.state.currentTask;
+    let tasks = [...this.state.tasks];
+    tasks.push(task)
+    this.setState({tasks: tasks});
+  };
+
+  changeCurrentTask = (task) => {
+    this.setState({currentTask: task})
+  };
+
+  render() {
+    const tasks = this.state.tasks.map(task =>
+      <Task key={task.id} id={task.id} deleteTask={this.deleteTask} title={task.text} />
+    );
+    return (
+        <div className="App">
+          <AddTaskForm addTask={this.addTask} title={this.state.currentTask} changeTask={this.changeCurrentTask}/>
+          {tasks}
+        </div>
+
+    );
+
+  }
+
 }
+
+
 
 export default App;
